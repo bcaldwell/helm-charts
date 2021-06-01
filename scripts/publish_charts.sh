e1590fd23506eb13cb7eafa40cbc388d8e4ecd1b
Benjamin Caldwell <8838361+bcaldwell@users.noreply.github.com>
2021-06-01 08:53:56 -0700

    Update publish_charts.sh

--- scripts/publish_charts.sh
+++ scripts/publish_charts.sh
@@ -55,7 +55,8 @@ lookup_latest_tag() {
     git fetch --tags > /dev/null 2>&1
 
     if ! git describe --tags --abbrev=0 2> /dev/null; then
-        git rev-list --max-parents=0 --first-parent HEAD
+#         git rev-list --max-parents=0 --first-parent HEAD
+         git rev-list -n 1 $(git describe --tags --abbrev=0)
     fi
 }
 
